6c28248cb3bcfa3364c3c373cb44ffb46d1e87b5
amNobodyyy <ksuhrud24@gmail.com>
2025-10-23 16:20:36 +0530

    port change

--- app.py
+++ app.py
@@ -186,4 +186,5 @@ def home():
     )
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's PORT or default to 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
